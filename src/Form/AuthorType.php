95931f4fdfb3c8fdac3fb02d4d30a2a2f724ec8f
charles <charles@sio.ndlp>
2026-09-17 15:43:56 +0200

    [Update] Visual and translation

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -26,7 +26,7 @@ class AuthorType extends AbstractType
             ])
             ->add('books', EntityType::class, [
                 'class' => Book::class,
-                'choice_label' => 'id',
+                'choice_label' => 'title',
                 'multiple' => true,
                 'required' => false, // required=false pour autoriser aucun livre
             ])
